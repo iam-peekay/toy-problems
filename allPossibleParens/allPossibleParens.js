69f636cfc4f7d002971792427a005a290ae33527
iam-peekay <iam.preethi.k@gmail.com>
2016-03-25 12:54:40 -0700

    Implement back tracking algo to all possible parens

---
+++ allPossibleParens/allPossibleParens.js
@@ -0,0 +1,28 @@
+/*
+
+Print all parentheses combinations for a given value n such that
+they are balanced.
+*/
+
+function printAllParens(n) {
+  var output = [];
+  printAllParensRecurse(n, 0, 0, output);
+}
+
+function printAllParensRecurse(n, right, left, output) {
+  if (right >= n && left >= n) {
+    console.log(output);
+  }
+
+  if (left < n) {
+    output.push('{');
+    printAllParensRecurse(n, right, left + 1, output);
+    output.pop();
+  }
+
+  if (right < left) {
+    output.push('}');
+    printAllParensRecurse(n, right + 1, left, output);
+    output.pop();
+  }
+}
